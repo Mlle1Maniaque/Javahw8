dbbffed49306469ebc908d0540371b270f069f99
Mlle Maniaque <Mlle1Maniaque@yandex.ru>
2023-04-29 12:53:23 +0300

    Initial commit

---
+++ src/main/java/org/example/Post.java
@@ -0,0 +1,14 @@
+package org.example;
+public class Post {
+    public String name;
+    public String surname;
+    public String patronymic;
+    public FormDate birthday;
+    public Post() {
+        FormDate birthday = new FormDate();
+    }
+    public String passport;
+    public String phone;
+    public boolean subscription;
+
+}
