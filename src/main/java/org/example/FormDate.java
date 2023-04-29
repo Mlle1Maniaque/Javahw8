8cf1b423cd1fdb9f8df94e19a371b1aeecb8eb38
Mlle Maniaque <Mlle1Maniaque@yandex.ru>
2023-04-29 12:56:20 +0300

    changed

--- src/main/java/org/example/FormDate.java
+++ src/main/java/org/example/FormDate.java
@@ -1,4 +1,5 @@
 package org.example;
+
 public class FormDate {
     public int day;
     public int month;
--- src/main/java/org/example/Post.java
+++ src/main/java/org/example/Post.java
@@ -1,4 +1,5 @@
 package org.example;
+
 public class Post {
     public String name;
     public String surname;
